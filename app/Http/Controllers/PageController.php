edcf1d06a82089476e4e6521403f125106911c36
Yevhenii Zhevak <e.zhevak@gmail.com>
2020-07-20 13:38:52 +0300

    add OpenGraph

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 use App\Page;
 use Artesaos\SEOTools\Facades\SEOMeta;
+use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Http;
 
@@ -22,6 +24,14 @@ class PageController extends Controller
         SEOMeta::setDescription($page["meta_description"]);
         SEOMeta::setCanonical(Url::current());
         SEOMeta::setKeywords($page["meta_keywords"]);
+        
+        SEOTools::opengraph()->addProperty('title', $page["title"]);
+        SEOTools::opengraph()->addProperty('description', $page["meta_description"]);
+        SEOTools::opengraph()->addProperty('site_name', 'Marseille Resort');
+        SEOTools::opengraph()->addProperty('locale', App::getLocale().'-UA');
+        SEOTools::opengraph()->addProperty('type', 'articles');
+        SEOTools::opengraph()->addProperty('url', Url::current());
+        SEOTools::opengraph()->addProperty('image', asset('storage/'.$page->image));
 
         return view('index',compact('page'));
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()).'-UA' }}" prefix="og: //ogp.me/ns#">
+<html lang="{{ str_replace('_', '-', app()->getLocale()).'-UA' }}" xmlns:og="http://ogp.me/ns#">
+  <head>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
