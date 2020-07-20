c8d6b6bf5d0fee6e9dc84f3b85761d6edc43c8aa
Yevhenii Zhevak <e.zhevak@gmail.com>
2020-07-20 09:36:49 +0300

    add multilang tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()).'-UA' }}" prefix="og: //ogp.me/ns#">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="{{ asset('assets/favicon.png') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+ 
+    @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+    <link rel="alternate" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}" hreflang="{{ $localeCode.'-UA'}}" />
+    @endforeach
+    
     {!! SEO::generate(true) !!}
 
     @include('layouts.header')
